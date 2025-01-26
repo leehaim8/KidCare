9a777d2951f002b002f726a20e976a7579fded8f
alon2004 <alonfisher200@gmail.com>
2025-01-26 12:32:35 +0200

    changing the menu bar using matirial ui

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,44 +1,168 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { FaUserFriends, FaCog, FaSignOutAlt,FaSearch } from "react-icons/fa";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  IconButton,
+  Divider,
+  Typography,
+} from "@mui/material";
+import {
+  Menu as MenuIcon,
+  Close as CloseIcon,
+  Home as HomeIcon,
+  Group as GroupIcon,
+  BarChart as BarChartIcon,
+  Autorenew as AutorenewIcon,
+  LibraryBooks as LibraryBooksIcon,
+  Search as SearchIcon,
+  Settings as SettingsIcon,
+  Logout as LogoutIcon,
+} from "@mui/icons-material";
 
 const HamburgerMenu = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        localStorage.removeItem("birthdayModalShown");
-        navigate("/login");
-    };
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("birthdayModalShown");
+    navigate("/login");
+  };
 
-    return (
-        <div>
-            {!isOpen && (<button className="menu-button" onClick={toggleMenu}>☰</button>)}
-            <div className={`hamburger-menu ${isOpen ? "open" : ""}`}>
-                <button className="close-button" onClick={toggleMenu}>✖</button>
-                <nav className="menu-nav">
-                    <ul className="top-links">
-                        <li><Link to="/HomePage">🏠 HomePage</Link></li>
-                        <li><Link to="/attendence"><FaUserFriends /> Children's Attendance</Link></li>
-                        <li><Link to="/WeeklyFeedback">📊 Weekly Feedback</Link></li>
-                        <li><Link to="/PeriodicFeedback">🔄 Periodic Feedback</Link></li>
-                        <li><Link to="/ResourceLibrary">📚 Resource Library</Link></li>
-                        <li><Link to="/Experts"><FaSearch /> External Professional Directory</Link></li>
+  return (
+    <div>
+      {/* Icon Button to Open Menu */}
+      <IconButton onClick={toggleMenu} color="inherit">
+        <MenuIcon />
+      </IconButton>
 
-                    </ul>
-                    <ul className="bottom-links">
-                        <li><button onClick={handleLogout}><FaSignOutAlt /> Log-out</button></li>
-                        <li><Link to="#"><FaCog /> Setting</Link></li>
-                    </ul>
-                </nav>
-            </div>
+      {/* Drawer Component */}
+      <Drawer anchor="left" open={isOpen} onClose={toggleMenu}>
+        <div className="drawer-container">
+          {/* Header Section with Close Button */}
+          <div className="drawer-header">
+            <Typography variant="h6">Menu</Typography>
+            <IconButton onClick={toggleMenu}>
+              <CloseIcon />
+            </IconButton>
+          </div>
+          <Divider className="menu-divider" />
+
+          {/* Top Navigation Links */}
+          <List>
+            <ListItem
+              button
+              component={Link}
+              to="/HomePage"
+              onClick={toggleMenu}
+              className="menu-link"
+            >
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="HomePage" />
+            </ListItem>
+            <ListItem
+              button
+              component={Link}
+              to="/attendence"
+              onClick={toggleMenu}
+              className="menu-link"
+            >
+              <ListItemIcon>
+                <GroupIcon />
+              </ListItemIcon>
+              <ListItemText primary="Children's Attendance" />
+            </ListItem>
+            <ListItem
+              button
+              component={Link}
+              to="/WeeklyFeedback"
+              onClick={toggleMenu}
+              className="menu-link"
+            >
+              <ListItemIcon>
+                <BarChartIcon />
+              </ListItemIcon>
+              <ListItemText primary="Weekly Feedback" />
+            </ListItem>
+            <ListItem
+              button
+              component={Link}
+              to="/PeriodicFeedback"
+              onClick={toggleMenu}
+              className="menu-link"
+            >
+              <ListItemIcon>
+                <AutorenewIcon />
+              </ListItemIcon>
+              <ListItemText primary="Periodic Feedback" />
+            </ListItem>
+            <ListItem
+              button
+              component={Link}
+              to="/ResourceLibrary"
+              onClick={toggleMenu}
+              className="menu-link"
+            >
+              <ListItemIcon>
+                <LibraryBooksIcon />
+              </ListItemIcon>
+              <ListItemText primary="Resource Library" />
+            </ListItem>
+            <ListItem
+              button
+              component={Link}
+              to="/Experts"
+              onClick={toggleMenu}
+              className="menu-link"
+            >
+              <ListItemIcon>
+                <SearchIcon />
+              </ListItemIcon>
+              <ListItemText primary="External Professional Directory" />
+            </ListItem>
+          </List>
+
+          <Divider className="menu-divider" />
+
+          {/* Bottom Links */}
+          <List>
+            <ListItem
+              button
+              onClick={handleLogout}
+              className="menu-link"
+            >
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary="Log-out" />
+            </ListItem>
+            <ListItem
+              button
+              component={Link}
+              to="#"
+              onClick={toggleMenu}
+              className="menu-link"
+            >
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItem>
+          </List>
         </div>
-    );
+      </Drawer>
+    </div>
+  );
 };
 
 export default HamburgerMenu;
